2ac0a32de41e304ac913f8e0e861431675265beb
Igoris Ivanovas <igoris.x@inbox.lt>
2021-08-01 12:57:35 +0300

    match

--- 06/ssss.php
+++ 06/ssss.php
@@ -1,35 +1,15 @@
 <?php
 
-$array = [];
-$arrayABC = range('A', 'Z');
+// match
 
-foreach (range(0, 9)as $col) {
-    foreach (range(0, rand(2, 20)) as $row) {
-        $array[$col][$row] = $arrayABC[rand(0,25)];
+$test = 111;
 
-    }
-    sort($array[$col]);
-}
+$result = match ($test) {
+    5 => 'five',
+    4 => 'four',
+    3 => 'three',
+    2 => 'two',
+    default => 'not in aprty',
+};
 
-echo '<pre>';
-
-//4
-$array4 = [];
-$arrayCheck = [];
-
-
-foreach ($array  as $key => $col) {
-    // if (count($array[$key]) > count($array4)) {
-    //     $array4 = [];
-    //     $array4 []= $array[$key];
-    // }
-}
-
-sort(count($array[$key]);
-
-echo count($array[0]);
-
-print_r($array);
-print_r($array4 );
-
-//hujet 
+echo $result;
