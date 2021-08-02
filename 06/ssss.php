08f8e536567c7409cffd79c6b8fb0c491be4716b
pacifist <82802986+igoris12@users.noreply.github.com>
2021-08-02 15:22:20 +0300

    kd1

--- 06/ssss.php
+++ 06/ssss.php
@@ -2,14 +2,31 @@
 
 // match
 
-$test = 111;
+// $test = 111;
 
-$result = match ($test) {
-    5 => 'five',
-    4 => 'four',
-    3 => 'three',
-    2 => 'two',
-    default => 'not in aprty',
-};
+// $result = match ($test) {
+//     5 => 'five',
+//     4 => 'four',
+//     3 => 'three',
+//     2 => 'two',
+//     default => 'not in aprty',
+// };
 
-echo $result;
+// echo $result;
+
+
+// $stinrg = 'as tu aa bb cc';
+// $stinrg2 = '';
+// $array = explode (' ',$stinrg);
+ 
+// for ($i = count($array)-1; $i > -1;  $i--) {
+//     if ($stinrg2 == '') {
+//         $stinrg2 = ' ';
+//     }
+
+//     $stinrg2 .= " $array[$i]";
+// } 
+
+// print_r($array);
+// echo '<br>';
+// echo $stinrg2;
