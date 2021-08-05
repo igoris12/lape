65358bd2e980844246cbec9d4b30d380f4ed9ef3
Igoris Ivanovas <igoris.x@inbox.lt>
2021-08-05 10:01:49 +0300

    day 9 post get

---
+++ 08/nd6/nd6.php
@@ -0,0 +1,18 @@
+<?php
+
+
+echo "<body style='background-color: red;'></body>"
+?>
+
+<body style='background-color: gray;'>
+    
+<form action="nd62.php" method="get">
+    <input type="submit" value="GET">
+
+</form>
+<form action="nd61.php" method="post">
+    <input type="submit" value="POST">
+</form>
+
+
+</body>
---
+++ 08/nd6/nd62.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo "<body style='background-color: green;'></body>"
+?>
+
+<form action="nd62.php" method="get">
+    <input type="submit" value="GET">
+
+</form>
+<form action="nd61.php" method="post">
+    <input type="submit" value="POST">
+</form>
