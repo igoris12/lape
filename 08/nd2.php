1d8e01a594a8a4258005e7bb9d5fa11d2833fab4
Igoris Ivanovas <igoris.x@inbox.lt>
2021-08-05 11:02:16 +0300

    kd 8

--- 08/nd2.php
+++ 08/nd2.php
@@ -1,13 +1,11 @@
+
 <?php
-
-
-
 
 echo '<a href="http://localhost/lape/08/nd2.php">pack</a>';
 
   if( count($_GET) != 0 ):
       $color = $_GET['color'];
-echo "<body style='background-color: $color;'></body> ";
+echo "<body style='background-color: #$color;'></body> ";
  else: 
  echo "<body style='background-color:black;'></body>";
 endif 
