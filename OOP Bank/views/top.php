ddb3d8e375bd04f9fbb3f352bbf0477ead1188d5
Igoris Ivanovas <igoris.x@inbox.lt>
2021-08-19 15:12:02 +0300

    oop bank start

---
+++ OOP Bank/views/top.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank opp</title>
+</head>
+<body>
+    <nav>
+        <a href="#">Creat new account</a>
+        <a href="#">List</a>
+
+    </nav>
+
+    <style>
+    body {
+        margin: 20px;
+    }
+
+    nav {
+        background: gray;
+        margin: 10px;
+        padding: 10px;
+    }
+
+    a {
+       text-decoration: none;
+       color: white; 
+       margin: auto 10px;
+    }
+
+       a:hover {
+           color: blue; 
+    }
+
+    input {
+        margin-bottom: 10px;
+    }
+    </style>
